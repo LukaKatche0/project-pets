ecd11995292a09eefeca1045649fcce5e7ffe8fd
LukaKatche <lukakatche@gmail.com>
2020-09-25 21:52:07 +0400

    added add functionality using localstorage

--- add.js
+++ add.js
@@ -5,9 +5,12 @@ const description = document.getElementById('description');
 const contactPersonName = document.getElementById('contactPersonName');
 const contactPersonNumber = document.getElementById('contactPersonNumber');
 
+// localStorage.setItem('hello', '3');
 // const newStatement = new Statement(titleInput.nodeValue, contactPersonName.value, contactPersonNumber.value, null, description.value, null, null, imageInput.value); 
-
+const statements = JSON.parse(localStorage.getItem('statements'));
 addBtn.addEventListener('click', () => {
     const newStatement = new Statement(titleInput.value, contactPersonName.value, contactPersonNumber.value, null, description.value, null, null, imageInput.value); 
-    console.log(newStatement);
+    statements.push(newStatement);
+    localStorage.setItem('statements', JSON.stringify(statements));
+    // window.location.href = "";
 });
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const petList = document.querySelector('.pet-list');
-
-const statement1 = new Statement('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
-const statementsArr = [statement1];
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.search-input');
+// const statement1 = new Statement('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+// const statement2 = new Statement('იყიდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+// const statement3 = new Statement('იყიდება თეთრი კნუტი, 3 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+// const statement4 = new Statement('იყიდება თეთრი კნუტი, 3 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+// const statementsArr = [statement1, statement2, statement3, statement4];
+const statementsAsString = localStorage.getItem('statements');
+const statementsAsArray = JSON.parse(statementsAsString);
+console.log(statementsAsString);
+console.log(statementsAsArray);
 
 const appendStatement = (statement) => {
     const statementTemplate = `
@@ -21,4 +29,21 @@ const appendAllStatements = (statements) => {
     }
 }
 
-appendAllStatements(statementsArr);
+searchBtn.addEventListener('click', () => {
+    petList.innerHTML = '';
+    if (searchInput.value === '') {
+        appendAllStatements(statementsArr);
+        return;
+    }
+    const filteredArr = statementsArr.filter((statement) => {
+        // if (statement.title === searchInput.value) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return statement.title.includes(searchInput.value);
+    });
+    appendAllStatements(filteredArr);
+});
+
+appendAllStatements(statementsAsArray);
