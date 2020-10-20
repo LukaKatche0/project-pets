bd84df853d5040686eea379a2d626cfd47ab0b63
LukaKatche <lukakatche@gmail.com>
2020-10-20 21:20:32 +0400

    delete on click

--- add.js
+++ add.js
@@ -4,6 +4,9 @@ const imageInput = document.getElementById('image');
 const description = document.getElementById('description');
 const contactPersonName = document.getElementById('contactPersonName');
 const contactPersonNumber = document.getElementById('contactPersonNumber');
+const h1 = document.getElementById('value');
+
+h1.innerHTML += localStorage.getItem('inputValue');
 
 addBtn.addEventListener('click', () => {
     const newStatement = new Statement(titleInput.value, contactPersonName.value, contactPersonNumber.value, null, description.value, null, null, imageInput.value); 
@@ -14,6 +17,18 @@ const addStatement = (statementParam) => {
     $.ajax({
         method: "POST",
         url: "http://localhost:3000/statements",
+        data: statementParam
+    })
+    .done(function (data) {
+        //გადამისამართება იმის შემდეგ რაც ჩავამატეთ განცხადება
+        // window.location.href = "";
+    });
+}
+
+const editStatement = (statementParam) => {
+    $.ajax({
+        method: "PUT",
+        url: `http://localhost:3000/statements/${statement.id}`,
         data: statementParam
     })
     .done(function (data) {
--- index.js
+++ index.js
@@ -3,17 +3,60 @@ const searchBtn = document.querySelector('.search-btn');
 const searchInput = document.querySelector('.search-input');
 const statementsAsString = localStorage.getItem('statements');
 const statementsAsArray = JSON.parse(statementsAsString);
+const btn = document.getElementById('btn');
+const input = document.getElementById('txt-input');
 let statements = [];
+
+localStorage.setItem('userName', 'luka');
+
+const obj = {
+    firstName: 'luka',
+    lastName: 'katcheishvli'
+};
+
+// const objAsString = JSON.stringify(obj);
+// localStorage.setItem('userObj', objAsString);
+
+// const userName = localStorage.getItem('userName');
+// console.log(userName);
+
+// const objFromLocalStorage = localStorage.getItem('userObj');
+// console.log(objFromLocalStorage);
+
+// const parsedObj = JSON.parse(objFromLocalStorage);
+// console.log(parsedObj);
+
+btn.addEventListener('click', () => {
+    localStorage.setItem('inputValue', input.value);
+})
 
 const getStatements = () => {
     $.ajax({
         method: "GET",
         url: "http://localhost:3000/statements",
     })
-    .done(function (data) {
-        statements = data;
-        appendAllStatements(data);
-    });
+        .done(function (data) {
+            statements = data;
+            appendAllStatements(data);
+            const deleteBtns = document.querySelectorAll('.delete-btn');
+            console.log(statements);
+            for (let i = 0; i < deleteBtns.length; i++) {
+                deleteBtns[i].addEventListener('click', () => {
+                    const statementId = parseInt(deleteBtns[i].getAttribute('statementId'));
+                    // const statement = statements.find((statement) => {
+                    //     return statement.id === statementId;
+                    // });
+                    deleteStatement(statementId);
+                })
+            }
+        });
+}
+
+function toggleButtonVisibility() {
+    const statementButtons = document.querySelectorAll('.statement-button');
+    for (let i = 0; i < statementButtons.length; i++) {
+        statementButtons[i].classList.toggle('hidden');
+    }
 }
 
 const deleteStatement = (id) => {
@@ -21,10 +64,10 @@ const deleteStatement = (id) => {
         method: "DELETE",
         url: `http://localhost:3000/statements/${id}`,
     })
-    .done(function (data) {
-        statements = data;
-        getStatements();
-    });
+        .done(function (data) {
+            statements = data;
+            getStatements();
+        });
 }
 
 getStatements();
@@ -37,6 +80,7 @@ const appendStatement = (statement) => {
                 <p class="description">${statement.description}</p>
                 <p class="author-info">ავტორის სახელი: ${statement.contactPersonName}</p>
                 <p class="author-info">საკონტაქტო ნომერი: ${statement.contactPersonNumber}</p>
+                <button class="delete-btn" statementId="${statement.id}">Delete Statement with Id: ${statement.id}</button>
     </div>`;
     petList.innerHTML += statementTemplate;
 }
